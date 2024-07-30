09df0fb391bf9af2fe9e94c17b4316b0a7ac21e0
rionTamamushi <>
2024-07-30 12:19:42 +0900

    サービス

--- src/main/java/com/example/service/FizzbuzzService.java
+++ src/main/java/com/example/service/FizzbuzzService.java
@@ -1,5 +1,31 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FizzbuzzService {
 
+	public String fizzbuzzService(int a) {
+		
+		int b = a + 100;
+		String result = "";
+		
+		if(a < 1 || a > 100) {
+			result = "1~100で入力してください";
+		}
+		
+		if(b % 5 == 0 && b % 3 == 0) {
+			result = "fizzbuzz";
+		} else if(b % 5 == 0) {
+			result = "fizz";
+		} else if(b % 3 == 0) {
+			result = "buzz";
+		} else {
+			result = "当てはまらないです";
+		}
+		
+		return result;
+		
+	}
 }
+
