01b0823c0283f25a44753bdc9269f6c1b3b1e82e
rionTamamushi <>
2024-07-30 17:49:57 +0900

    ServiceImpl

--- src/main/java/com/example/service/impl/FizzbuzzServiceImpl.java
+++ src/main/java/com/example/service/impl/FizzbuzzServiceImpl.java
@@ -1,5 +1,23 @@
 package com.example.service.impl;
 
-public class FizzbuzzServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.model.FModel;
+import com.example.repository.FizzbuzzMapper;
+import com.example.service.FizzbuzzService;
+
+@Service
+public class FizzbuzzServiceImpl implements FizzbuzzService {
+	
+	@Autowired
+	private FizzbuzzMapper mapper;
+	
+	@Override
+	public List<FModel> findNum() {
+		return mapper.findNum();
+	}
+	
 }
